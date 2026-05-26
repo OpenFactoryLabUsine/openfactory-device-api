fdc50cdc57a6763d851cb0dc5eafebee4f0be575
camelia-d-e <cades413@ulaval.ca>
2026-05-25 22:15:02 -0400

    add logs in stream creation

--- services/stream_service.py
+++ services/stream_service.py
@@ -9,6 +9,8 @@ class StreamService:
 
     def create_equipment_stream(self, asset_uuid: str) -> str:
         topic_name = f"{asset_uuid}_MONITORING".upper()
+        print(f"Creating stream EQUIPMENT_STREAM_{asset_uuid} on topic {topic_name}")
+        asset_uuid = asset_uuid.upper()
         try:
             self._ksql_client.statement_query(
                 f"CREATE STREAM IF NOT EXISTS EQUIPMENT_STREAM_{asset_uuid} "
@@ -16,7 +18,7 @@ class StreamService:
                 f"SELECT ASSET_UUID AS KEY, ID, VALUE, "
                 f"TIMESTAMPTOSTRING(ROWTIME, 'yyyy-MM-dd''T''HH:mm:ss[.nnnnnnn]', 'Canada/Eastern') AS TIMESTAMP "
                 f"FROM ASSETS_STREAM WHERE ASSET_UUID = '{asset_uuid}' "
-                f"AND TYPE IN ('Events', 'Condition', 'Samples') AND VALUE != 'UNAVAILABLE' "
+                f"AND TYPE IN ('Samples')"
                 f"EMIT CHANGES;"
             )
             return topic_name
